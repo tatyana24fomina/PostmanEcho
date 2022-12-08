6dc3c542b12eecd21776d1b8479bb617c590b915
Tatyana Fomina <tatyana24fomina@gmail.com>
2022-12-09 00:27:27 +0400

    failed build

--- src/test/java/ru/netology/rest/MobileBankApiTest.java
+++ src/test/java/ru/netology/rest/MobileBankApiTest.java
@@ -20,6 +20,6 @@ public class MobileBankApiTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"));
+                .body("data", equalTo("hello"));
     }
 }
